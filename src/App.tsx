131254c831124b00df6a5c10c4bc1ef98a97d365
nyyanja <nyanja.ramahavatra@gmail>
2026-05-29 11:28:10 +0300

    feat: configuration initiale React-Admin avec dataProvider

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Admin, Resource } from "react-admin";
+import jsonServerProvider from "ra-data-json-server";
+import { EmployeeList } from "./employees/EmployeeList";
+import { EmployeeCreate } from "./employees/EmployeeCreate";
+import { EmployeeEdit } from "./employees/EmployeeEdit";
+import { EmployeeShow } from "./employees/EmployeeShow";
+
+const dataProvider = jsonServerProvider("http://localhost:3002");
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="employees"
+      list={EmployeeList}
+      create={EmployeeCreate}
+      edit={EmployeeEdit}
+      show={EmployeeShow}
+    />
+  </Admin>
+);
+
+export default App;
