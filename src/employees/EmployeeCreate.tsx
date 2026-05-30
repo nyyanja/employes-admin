bcfd3e0ff1b3867a601c933f68345447e6c8e70c
nyyanja <nyanja.ramahavatra@gmail>
2026-05-30 09:11:18 +0300

    Fix(edit): add type = email

--- src/employees/EmployeeCreate.tsx
+++ src/employees/EmployeeCreate.tsx
@@ -14,7 +14,7 @@ export const EmployeeCreate = () => (
     <SimpleForm>
       <TextInput source="firstname" label="Prénom" validate={required()} />
       <TextInput source="lastname" label="Nom" validate={required()} />
-      <TextInput source="email" label="Email" validate={required()} />
+      <TextInput source="email" label="Email" type="email" validate={required()} />
       <SelectInput
         source="department"
         label="Département"
@@ -30,6 +30,7 @@ export const EmployeeCreate = () => (
         source="salary"
         label="Salaire"
         validate={[required(), minValue(1500)]}
+        min={1500}
       />
       <BooleanInput source="active" label="Actif" defaultValue={true} />
     </SimpleForm>
