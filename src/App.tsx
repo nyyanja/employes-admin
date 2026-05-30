926581a1d59f069dd00c52afc1192e2953a1e6f1
nyyanja <nyanja.ramahavatra@gmail>
2026-05-30 09:54:18 +0300

    feat: register interns resource in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, EditGuesser, ShowGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { EmployeeList } from "./employees/EmployeeList";
 import { EmployeeCreate } from "./employees/EmployeeCreate";
 import { EmployeeEdit } from "./employees/EmployeeEdit";
 import { EmployeeShow } from "./employees/EmployeeShow";
+import { InternCreate } from "./interns/InternCreate";
+import { InternList } from "./interns/InternList";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
@@ -15,6 +17,13 @@ const App = () => (
       create={EmployeeCreate}
       edit={EmployeeEdit}
       show={EmployeeShow}
+    />
+    <Resource
+      name="interns"
+      list={InternList}
+      create={InternCreate}
+      edit={EditGuesser}
+      show={ShowGuesser}
     />
   </Admin>
 );
