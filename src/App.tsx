02ce0a5698082aea58cfb7a9d245450bd2421406
nyyanja <nyanja.ramahavatra@gmail>
2026-06-03 09:54:46 +0300

    feat: register dashboard and update resources in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Admin, Resource, EditGuesser, ShowGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { EmployeeList } from "./employees/EmployeeList";
 import { EmployeeCreate } from "./employees/EmployeeCreate";
 import { EmployeeEdit } from "./employees/EmployeeEdit";
 import { EmployeeShow } from "./employees/EmployeeShow";
-import { InternCreate } from "./interns/InternCreate";
 import { InternList } from "./interns/InternList";
+import { InternCreate } from "./interns/InternCreate";
+import { InternEdit } from "./interns/InternEdit";
+import { InternShow } from "./interns/InternShow";
+import { Dashboard } from "./Dashboard";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource
       name="employees"
       list={EmployeeList}
@@ -22,8 +25,8 @@ const App = () => (
       name="interns"
       list={InternList}
       create={InternCreate}
-      edit={EditGuesser}
-      show={ShowGuesser}
+      edit={InternEdit}
+      show={InternShow}
     />
   </Admin>
 );
